f23343df9d765b7a3b7c12321289b6bae863b66f
Nathan Fleet <nfleet@kent.edu>
2023-03-26 14:36:57 -0400

    edit

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -4,8 +4,8 @@ const Section = () => {
   return (
     <div className="bg-hero-image w-full h-screen bg-cover bg-center flex items-center px-4">
         <div className="uppercase text-center text-white font-extrabold">
-            <h1 className="text-8xl">Nathan Fleet</h1>
-            <h3>Portfolio</h3>
+            <h1 className="text-8xl">Hi, my name is Nathan</h1>
+            <h3>Welcome to my portfolio</h3>
         </div>
     </div>
   )
