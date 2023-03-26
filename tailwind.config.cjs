50f89bc4f2070dc4d5bc4a7b399dd0be1197a163
Nathan Fleet <nfleet@kent.edu>
2023-03-26 01:02:29 -0400

    minor changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
+  mode: "jit",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#151030",
+        secondary: "#aaa6c3",
+        tertiary: "#f3f3f3",
+      },
+      backgroundImage: {
+        "hero-pattern": "url('src/assets/herobg.png')"
+      }
+    },
   },
   plugins: [],
 }
