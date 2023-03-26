49311d66d73c266e4d5facd35fe0666b7b5db371
Nathan Fleet <nfleet@kent.edu>
2023-03-26 14:33:17 -0400

    menu icon not visible on md + screens

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,30 @@
 import { MenuOutlined } from '@material-ui/icons'
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const Header = () => {
-    const [active, setActive] = useState(false)
-    const showMenu = () => {
-        setActive(!active)
-    }
+  const [active, setActive] = useState(false)
+  const showMenu = () => {
+    setActive(!active)
+  }
 
   return (
     <div className="fixed w-full text-white flex justify-between px-4 items-center">
+      <div>
+        logo
+      </div>
 
-        <div>
-            logo
+      <nav>
+        <div className="md:hidden">
+          <MenuOutlined onClick={showMenu} />
         </div>
 
-        <nav>
-
-            <div className>
-                <MenuOutlined onClick={showMenu}/>
-            </div>
-
-            <ul className="hidden md:flex gap-8 p-6 uppercase bg-white/10">
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/'>About</Link></li>
-                <li><Link to='/'>Contact</Link></li>
-            </ul>
-
-
-            
-        </nav>
-
+        <ul className={`md:flex md:gap-8 md:p-6 md:uppercase bg-white/10 ${active ? 'block' : 'hidden'}`}>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/'>About</Link></li>
+          <li><Link to='/'>Contact</Link></li>
+        </ul>
+      </nav>
     </div>
   )
 }
