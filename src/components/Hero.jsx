d18d48bae128ad374860a9e69db1a198a8deb318
Nathan Fleet <nfleet@kent.edu>
2023-03-26 21:25:41 -0400

    added favicon, changes to navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Section = () => {
   return (
     <div className="bg-hero-image w-full h-screen bg-cover bg-center flex items-center px-4">
-        <div className="uppercase text-center text-white font-extrabold">
+        <div className="uppercase text-white font-extrabold justify-start">
             <h1 className="text-8xl">Hi, i'm Nathan</h1>
             <h3>Welcome to my portfolio</h3>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,41 @@
-import { MenuOutlined } from '@material-ui/icons'
+import { MenuOutlined, Close } from '@material-ui/icons'
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import logo from '../assets/logo.png'
 
 const Navbar = () => {
   const [active, setActive] = useState(false)
-  const showMenu = () => {
+
+  const toggleMenu = () => {
     setActive(!active)
   }
 
   return (
     <div className="fixed w-full text-white flex justify-between px-4 items-center">
-      <div>
-        logo
-      </div>
-
+      <Link to='/'>
+        <img src={logo} alt="logo" className="w-20 h-20 object-contain" />
+      </Link>
       <nav>
-        <div className="md:hidden">
-          <MenuOutlined onClick={showMenu} />
-        </div>
-
-        <ul className={`md:flex md:gap-8 md:p-6 md:uppercase bg-white/10 ${active ? 'block' : 'hidden'}`}>
+        <ul className="hidden md:flex md:gap-8 md:p-6 md:uppercase bg-white/10">
           <li><Link to='/'>Home</Link></li>
           <li><Link to='/'>About</Link></li>
+          <li><Link to='/'>Projects</Link></li>
           <li><Link to='/'>Contact</Link></li>
         </ul>
+
+        {active ? (
+          <ul className="md:hidden flex-col flex left-1/4 inset-0 fixed uppercase items-center justify-center p-8 backdrop-blur-lg gap-8 bg-black/40">
+            <Close onClick={toggleMenu} className="cursor-pointer" />
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/'>About</Link></li>
+            <li><Link to='/'>Projects</Link></li>
+            <li><Link to='/'>Contact</Link></li>
+          </ul>
+        ) : (
+          <div className="md:hidden">
+            <MenuOutlined onClick={toggleMenu} className="cursor-pointer" />
+          </div>
+        )}
       </nav>
     </div>
   )
