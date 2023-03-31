1bfcd895c10c202aae273178705fc8a7322b1266
Nathan Fleet <nfleet@kent.edu>
2023-03-31 15:16:27 -0400

    minor changes to about, skills section complete

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { motion } from 'framer-motion';
-import { textVariant, fadeIn } from '../utils/motion';
 import { styles } from '../constants';
+import { motion } from 'framer-motion';
 import github from '../assets/github.png';
 import linkedin from '../assets/linkedin.png';
 
@@ -9,23 +8,22 @@ const About = () => {
   return (
     <div className="w-full h-screen flex items-center justify-center">
       <div className="max-w-4xl w-full px-4">
-        <motion.div variants={textVariant()} className="flex-col items-left">
+        <div className="flex-col items-left">
           <p className={styles.subText}>Introduction</p>
           <h2 className={styles.headText}>Overview.</h2>
-        </motion.div>
+        </div>
 
-        <motion.p variants={fadeIn("", "", 0.1, 1)} 
-        className='mt-4 text-secondary text-lg max-w-3xl leading-[30px] text-left text-white'>
+        <p className='mt-4 text-secondary text-lg max-w-3xl leading-[30px] text-left text-white'>
           I'm a computer science student at Kent State University, currently enrolled in the honors 
           college program. My academic interests lie in programming languages such as Python, JavaScript, 
           and C++, and I enjoy using them to solve real-world problems. I'm passionate about software 
           development and continuously strive to enhance my skills through hands-on experience and challenging 
           coursework. I'm committed to developing innovative solutions that contribute to the betterment of society,
            and I'm excited about the opportunities that lie ahead as I continue to pursue my academic and professional goals.
-        </motion.p>
+        </p>
 
         <div className="flex justify-center mt-4">
-        <motion.a
+          <motion.a
             href="https://github.com/nathanfleet03"
             target="_blank"
             rel="noopener noreferrer"
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+import { styles } from '../constants';
 
 import cplus from '../assets/tech/cplus.png';
 import git from '../assets/tech/git.png';
@@ -10,29 +12,48 @@ import tailwind from '../assets/tech/tailwind.png';
 
 const Skills = () => {
   return (
-    <div className="bg-white rounded-lg shadow-md p-6">
-      <h2 className="text-2xl font-bold mb-4">Things I Use</h2>
-      <div className="flex flex-wrap items-center justify-center">
-        <div className="m-4">
-          <img src={cplus} alt="C++ logo" className="w-16 h-16 mx-auto" />
+    <div className="bg-black text-white h-1/2 flex flex-col items-center justify-center md:h-full">
+      <div className="max-w-4xl w-full px-4">
+        <div className="flex-col items-left text-left">
+          <p className={styles.subText}>My experience with technologies</p>
+          <h2 className={styles.headText}>Tech.</h2>
         </div>
-        <div className="m-4">
-          <img src={git} alt="Git logo" className="w-16 h-16 mx-auto" />
-        </div>
-        <div className="m-4">
-          <img src={html} alt="HTML logo" className="w-16 h-16 mx-auto" />
-        </div>
-        <div className="m-4">
-          <img src={javascript} alt="JavaScript logo" className="w-16 h-16 mx-auto" />
-        </div>
-        <div className="m-4">
-          <img src={python} alt="Python logo" className="w-16 h-16 mx-auto" />
-        </div>
-        <div className="m-4">
-          <img src={react} alt="React logo" className="w-16 h-16 mx-auto" />
-        </div>
-        <div className="m-4">
-          <img src={tailwind} alt="Tailwind logo" className="w-16 h-16 mx-auto" />
+        <div className="flex flex-wrap items-center justify-center">
+          <div className="m-4">
+            <motion.div whileHover={{ scale: 1.5 }}>
+              <img src={cplus} alt="C++ logo" className="w-20 h-20 mx-auto" />
+            </motion.div>
+          </div>
+          <div className="m-4">
+            <motion.div whileHover={{ scale: 1.5 }}>
+              <img src={git} alt="Git logo" className="w-20 h-20 mx-auto" />
+            </motion.div>
+          </div>
+          <div className="m-4">
+            <motion.div whileHover={{ scale: 1.5 }}>
+              <img src={html} alt="HTML logo" className="w-20 h-20 mx-auto" />
+            </motion.div>
+          </div>
+          <div className="m-4">
+            <motion.div whileHover={{ scale: 1.5 }}>
+              <img src={javascript} alt="JavaScript logo" className="w-20 h-20 mx-auto" />
+            </motion.div>
+          </div>
+          <div className="m-4">
+            <motion.div whileHover={{ scale: 1.5 }}>
+              <img src={python} alt="Python logo" className="w-20 h-20 mx-auto" />
+            </motion.div>
+          </div>
+          <div className="m-4">
+            <motion.div whileHover={{ scale: 1.5 }}>
+              <img src={react} alt="React logo" className="w-20 h-20 mx-auto" />
+            </motion.div>
+          </div>
+          <div className="m-4">
+            <motion.div whileHover={{ scale: 1.5 }}>
+              <img src={tailwind} alt="Tailwind logo" className="w-20 h-20 mx-auto" />
+            </motion.div>
+          </div>
         </div>
       </div>
     </div>
