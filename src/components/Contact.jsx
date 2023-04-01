a9f2f7eacc53b82de89eff7f0d0eca5044f9f2f6
Nathan Fleet <nfleet@kent.edu>
2023-03-31 21:54:56 -0400

    Added contact section with emailjs

---
+++ src/components/Contact.jsx
@@ -0,0 +1,123 @@
+import React, { useRef, useState } from "react";
+
+import emailjs from '@emailjs/browser';
+
+import { styles } from '../constants';
+
+const Contact = () => {
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    const { target } = e;
+    const { name, value } = target;
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        'service_97zdspg',
+        'template_wsjf2t8',
+        {
+          from_name: form.name,
+          to_name: "Nathan Fleet",
+          from_email: form.email,
+          to_email: "nfleet@kent.edu",
+          message: form.message,
+        },
+        'STtC_ifqyaR3D0K5N'
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank you. I will get back to you as soon as possible.");
+
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.error(error);
+
+          alert("Ahh, something went wrong. Please try again.");
+        }
+      );
+  };
+
+  return (
+    <div
+      className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden items-center justify-center`}
+    >
+      <div className='flex-[0.75] bg-black p-8 rounded-2xl max-w-4xl w-full px-4 items-left text-left'>
+        <p className={styles.subText}>Get in touch</p>
+        <h3 className={styles.headText}>Contact.</h3>
+
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className='mt-12 flex flex-col gap-8'
+        >
+          <label className='flex flex-col'>
+            <span className='text-white font-medium mb-4'>Your Name</span>
+            <input
+              type='text'
+              name='name'
+              value={form.name}
+              onChange={handleChange}
+              placeholder="What's your name?"
+              className='py-4 px-6 text-black rounded-lg outline-none border-none font-medium'
+            />
+          </label>
+          <label className='flex flex-col'>
+            <span className='text-white font-medium mb-4'>Your email</span>
+            <input
+              type='email'
+              name='email'
+              value={form.email}
+              onChange={handleChange}
+              placeholder="What's your web address?"
+              className='py-4 px-6 text-black rounded-lg outline-none border-none font-medium'
+            />
+          </label>
+          <label className='flex flex-col'>
+            <span className='text-white font-medium mb-4'>Your Message</span>
+            <textarea
+              rows={7}
+              name='message'
+              value={form.message}
+              onChange={handleChange}
+              placeholder='What do you want to say?'
+              className='py-4 px-6 text-black rounded-lg outline-none border-none font-medium'
+            />
+          </label>
+
+          <button
+            type='submit'
+            className='bg-blue-400 py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
+          >
+            {loading ? "Sending..." : "Send"}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
