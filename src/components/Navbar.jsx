2dccd986fcb4eff225897e032b36e3bf8d95a3cb
Nathan Fleet <nfleet@kent.edu>
2023-04-01 20:12:32 -0400

    navbar fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,14 +12,17 @@ const Navbar = () => {
   }
 
   return (
-    <div className="fixed w-full text-white flex justify-between px-4 items-center">
+    <div className="fixed w-full text-black flex justify-between px-4 items-center bg-white">
 
+      <div className="flex items-center">
       <Link to='/'
       onClick={() => {
         window.scrollTo(0, 0);
       }}>
         <img src={logo} alt="logo" className="w-20 h-20 object-contain" />
       </Link>
+      <span className="ml-2 text-xl">Nathan</span>
+      </div>
 
       <nav>
         <ul className="hidden md:flex md:gap-8 md:p-6 md:uppercase">
@@ -31,7 +34,7 @@ const Navbar = () => {
         </ul>
 
         {active ? (
-          <ul className="md:hidden flex-col flex left-1/4 inset-0 fixed uppercase items-center justify-center p-8 backdrop-blur-lg gap-8">
+          <ul className="md:hidden flex-col flex left-1/4 inset-0 fixed uppercase items-center justify-center p-8 backdrop-blur-lg gap-8 text-white">
             <Close onClick={toggleMenu} className="cursor-pointer" />
             {navLinks.map((link) => (
               <li key={link.id} className="hover:text-blue-400">
