c0f5cb1018e870c2600c3a6ce046d103a01822e2
Nathan Fleet <nfleet@kent.edu>
2023-05-15 18:48:17 -0400

    added resume to home page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { styles } from '../constants'
+import resume from '../assets/resume.pdf'
 
 const Hero = () => {
   return (
     <div className="w-full h-screen flex items-center justify-center px-4">
-        <div className=" text-white font-extrabold">
+        <div className="text-white font-extrabold">
             <h1 className={styles.headText}>Hi, i'm <span className="text-blue-400">Nathan</span></h1>
             <h3 className={styles.subText}>Welcome to my portfolio</h3>
+            <div className="flex items-center justify-center mt-10">
+                <a href={resume} target="_blank" rel="noopener noreferrer">
+                    <button className="mr-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        My Resume
+                    </button>
+                </a>
+            </div>
         </div>
     </div>
   )
